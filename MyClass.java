5997666a153388c16cdb3d64dbaf865a0d6fdf3e
Balasubramanyam <balasubramanip2002@gmail.com>
2023-12-16 14:26:03 +0530

    added

--- MyClass.java
+++ MyClass.java
@@ -1,6 +1,11 @@
 class MyClass {
+	int x;
+	int y;
   void add(int x, int y)  //adding two integers 
   {
     System.out.println("Sum of two numbers: " &x+y);
   }
-}
+  public static void Main(String Arr[]){
+	  MyClass m = new MyClass();
+	  m.add(10,10)
+}}
