15b1bbc01176593e219210518092864c23fd96b6
Abishek S Balasubramani <abishek.b@itp.objectfrontier.com>
2023-12-16 14:12:09 +0530

    committed

--- MyClass.java
+++ MyClass.java
@@ -3,4 +3,12 @@ class MyClass {
   {
     System.out.println("Sum of two numbers: " &x+y);
   }
+  void sub(int x, int y) //adding two integers
+	{
+		System.out.println("subtraction of two numbers: " &x-y);
+	}
+  void div(int x, int y) 
+  {
+	  System.out.println("division of two numbers: " &x/y);
+  }
 }
